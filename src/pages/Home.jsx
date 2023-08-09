c5e5fb3dd22e8a5e229dc023e8d28ca0dbc22a3d
ngf93 <nabiullinaguzel93@gmail.com>
2023-08-09 19:58:27 +0300

    adaptive

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,7 @@ import Col from 'react-bootstrap/Col';
 import useObserver from '../hooks/useObserver';
 
 import Offer from '../components/Offer';
+import Company from '../components/Company';
 
 /* icons & images */
 import Flash from '../components/svg/Flash';
@@ -29,6 +30,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/effect-fade';
 import 'swiper/css/autoplay';
+
 
 const Home = () => {
   const [objRef, isVisible] = useObserver({threshold: 0.5});
@@ -94,7 +96,7 @@ const Home = () => {
       <section className='sec-3 mb-6'>
         <Container>
           <h2>Кому подойдёт сервис?</h2>
-          <Row md={4} className='gx-5'>
+          <Row xs={2} md={3} lg={4} className='justify-content-center g-2 g-sm-3 g-xl-5'>
             <Col>
               <figure>
                 <img src="/imgs/img1.png" alt="Кафе и ресторанам" />
@@ -125,14 +127,14 @@ const Home = () => {
 
       <section className='sec-4 mb-6'>
         <Container>
-          <div className="top mb-4">
+          <div className="top mb-2 mb-sm-4">
             <h3 className='fw-7 mb-0'>Заголовок о том, что входит в предложение</h3>
-            <a href="" className='d-flex align-items-center'>
+            <a href="" className='d-none d-lg-flex align-items-center'>
               <Start className="fs-15"/>
               <span className='fs-11 ms-2'>Посмотреть демо-версию</span>
             </a>
           </div>
-          <ul className="list-unstyled row row-cols-2 g-4">
+          <ul className="list-unstyled row row-cols-1 row-cols-md-2 g-2 g-sm-4">
             <li>
               <Offer 
                 title={'Мобильное приложение'}
@@ -167,12 +169,14 @@ const Home = () => {
 
       <Container>
         <section className='sec-5 mb-6'>
-          <img src="/imgs/img5.png" alt="img5" className='sec-5-img'/>
           <div className="row">
-            <div className="col-7">
+            <div className="col-12 col-lg-8 col-xl-7">
               <h2>Пройдите опрос и получите <br/>скидку <span>— 10 тыс. ₽</span> на запуск</h2>
               <p>Нужен ещё текст про условия акции или что‑то, что расказывает о выгодне прохождения этого опроса</p>
-              <button type='button' className='btn-info mt-5'>
+              <div className='sec-5-img'>
+                <img src="/imgs/img5.png" alt="img5"/>
+              </div>
+              <button type='button' className='btn-info w-xs-100 mt-4 mt-lg-5'>
                 <Flash className="fs-12"/>
                 <span className='ms-2'>Пройти опрос</span>
               </button>
@@ -184,55 +188,56 @@ const Home = () => {
       <section className='sec-6 mb-6'>
         <Container className='wide'>
           <h2 className='text-start'>Что то очень интересное в заголовке</h2>
-          <div className="row g-4">
-            <div className="col-5">
+          <Row className="g-2 g-xl-4">
+            <Col xs={12} lg={5}>
               <div className="grayBlock-1">
                 <h3 className='text-uppercase deepblue mb-2'>старт</h3>
                 <h4>Подключение YooApp</h4>
                 <p>Внедрим сайт и мобильное приложение за 2 недели. Интегрируем с системой управления заведением, настроим зоны доставки и подключим к онлайн-оплате.</p>
                 <BtnPuls/>
               </div>
-            </div>
-            <div className="col-7">
-              <div className="row row-cols-2 g-4">
-                <div>
+            </Col>
+            <Col xs={12} lg={7}>
+              <Row xs={1} sm={2} className="g-2 g-xl-4">
+                <Col>
                   <div className="grayBlock-2">
                     <h3 className='text-uppercase deepblue mb-2'>01</h3>
                     <h4>Клиент совершает заказ</h4>
                     <p>Удобное меню и простота оформления обеспечат быстрый заказ как на доставку, так и в заведении. А умная корзина и функционал модификации товаров повысят средний чек.</p>
                   </div>
-                </div>
-                <div>
+                </Col>
+                <Col>
                   <div className="grayBlock-3">
                     <h3 className='text-uppercase deepblue mb-2'>02</h3>
                     <h4>Заказ поступает в систему управления заведением</h4>
                     <p>iiko, r_keeper, frontpad и т.д. Если у вас нет подобной системы, то мы настроим управление через админ-панель Yoo.App или поможем внедрить.</p>
                   </div>
-                </div>
-                <div>
+                </Col>
+                <Col>
                   <div className="grayBlock-4">
                     <h3 className='text-uppercase deepblue mb-2'>04</h3>
                     <h4>Один раз клиент — всегда клиент</h4>
                     <p>Отправьте клиенту персональное предложение на основе его заказов, управляйте акциями и скидками, и он придёт к вам снова.</p>
                   </div>  
-                </div>
-                <div>
+                </Col>
+                <Col>
                   <div className="grayBlock-5">
                     <h3 className='text-uppercase deepblue mb-2'>03</h3>
                     <h4>Взаимодействие с пользователем</h4>
                     <p>Клиент будет получать уведомления о каждой смене статуса заказа. Если доставка задерживается, система уведомит его, сохранив лояльность. А после завершения соберёт обратную связь и попросит оставить отзыв.</p>
                   </div>
-                </div>
-              </div>
-            </div>
-          </div>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
         </Container>
       </section>
 
-      <section className='sec-7 px-5 mb-6'>
+      <section className='sec-7 mb-6'>
         <Container>
           <h2 className='text-start'>Сихронизируемся с любой системой по API</h2>
-          <img src="/imgs/scheme.png" alt="scheme" className='img-fluid'/>
+          <img src="/imgs/scheme.png" alt="scheme" className='d-none d-md-block img-fluid mx-auto'/>
+          <img src="/imgs/schemeMobile.png" alt="scheme" className='d-block d-md-none img-fluid mx-auto'/>
         </Container>
       </section>
 
@@ -260,52 +265,52 @@ const Home = () => {
             }}
           >
             <SwiperSlide>
-              <Row lg={2}>
+              <Row xs={1} md={2} className='flex-md-row-reverse g-4'>
+                <Col>
+                  <div className='textBox'>
+                    <img src="/imgs/img7.png" alt="Система лояльности"/>
+                    <h3>Система лояльности есть, даже если её нет</h3>
+                    <p>Мы продумали систему лояльности для заведений которые пользуются сервисами без системы лояльности.</p>
+                  </div>
+                </Col>
                 <Col>
                   <div className="img">
                     <img src="/imgs/img6.png" alt="Система лояльности" className='img-main' ref={objRef}/>
                     <img src="/imgs/img8.png" alt="Система лояльности" className='img-popup' data-observing={isVisible}/>
                   </div>
                 </Col>
-                <Col>
-                  <div className='textBox'>
-                    <img src="/imgs/img7.png" alt="Система лояльности" className='mb-5'/>
-                    <h3>Система лояльности есть, <br/>даже если её нет</h3>
-                    <p>Мы продумали систему лояльности для заведений которые пользуются сервисами без системы лояльности.</p>
-                  </div>
-                </Col>
               </Row>
             </SwiperSlide>
             <SwiperSlide>
-              <Row lg={2}>
+              <Row xs={1} md={2} className='flex-md-row-reverse g-4'>
+                <Col>
+                  <div className='textBox'>
+                    <img src="/imgs/img10.png" alt="Таргетинг добрался и до Push"/>
+                    <h3>Таргетинг добрался и до Push</h3>
+                    <p>Отсортируйте пользователей, по дню рождения, зоне доставки или любимому блюду, и отправьте им уведомление о ваших акциях.</p>
+                  </div>
+                </Col>
                 <Col>
                   <div className="img2">
                     <img src="/imgs/img9.png" alt="Таргетинг добрался и до Push" className='img2-main' ref={objRef2}/>
                     <img src="/imgs/img11.png" alt="Таргетинг добрался и до Push" className='img2-popup' data-observing={isVisible2}/>
                   </div>
                 </Col>
-                <Col>
-                  <div className='textBox'>
-                    <img src="/imgs/img10.png" alt="Таргетинг добрался и до Push" className='mb-5'/>
-                    <h3>Таргетинг добрался и до Push</h3>
-                    <p>Отсортируйте пользователей, по дню рождения, зоне доставки или любимому блюду, и отправьте им уведомление о ваших акциях.</p>
-                  </div>
-                </Col>
               </Row>
             </SwiperSlide>
             <SwiperSlide>
-              <Row lg={2}>
+              <Row xs={1} md={2} className='flex-md-row-reverse g-4'>
+                <Col>
+                  <div className='textBox'>
+                    <img src="/imgs/img13.png" alt="Настроим зоны доставок"/>
+                    <h3>Настроим зоны доставок</h3>
+                    <p>С нами вы сможете разделить доставку по зонам, исходя из этого настроим автоматический расчёт стоимости доставки, суммы бесплатной доставки и минимального чека.</p>
+                  </div>
+                </Col>
                 <Col>
                   <div className="img3">
                     <img src="/imgs/img12.png" alt="Настроим зоны доставок" className='img3-main' ref={objRef3}/>
                     <img src="/imgs/img14.png" alt="Настроим зоны доставок" className='img3-popup' data-observing={isVisible3}/>
-                  </div>
-                </Col>
-                <Col>
-                  <div className='textBox'>
-                    <img src="/imgs/img13.png" alt="Настроим зоны доставок" className='mb-5'/>
-                    <h3>Настроим зоны доставок</h3>
-                    <p>С нами вы сможете разделить доставку по зонам, исходя из этого настроим автоматический расчёт стоимости доставки, суммы бесплатной доставки и минимального чека.</p>
                   </div>
                 </Col>
               </Row>
@@ -388,7 +393,7 @@ const Home = () => {
 
       <section className='sec-12 mb-6'>
         <Container className='wide'>
-          <Row className='g-4'>
+          <Row className='g-3 g-xl-4'>
             <Col md={4}>
               <h3>Выберите план развития вашего бизнеса</h3>
             </Col>
@@ -510,7 +515,7 @@ const Home = () => {
         </Container>
       </section>
 
-      <section className='sec-13 mb-6'>
+      <section className='d-none d-lg-block sec-13 mb-6'>
         <Container>
           <h2 className='text-start ps-5'>Результат реального клиента</h2>
           <div className="diagram">
@@ -531,39 +536,41 @@ const Home = () => {
           <h2>Компании, которые уже пользуются сервисом</h2>
           <Swiper
             spaceBetween={11}
-            slidesPerView={4}
+            slidesPerView={'auto'}
+            breakpoints={{
+              768: {
+                slidesPerView: 3,
+                spaceBetween: 11,
+              },
+              992: {
+                slidesPerView: 4,
+                spaceBetween: 11,
+              },
+            }}
           >
             <SwiperSlide>
-              <img src="/imgs/companies/totos-logo.png" alt="totos" className='img-fluid d-block mx-auto'/>
-              <img src="/imgs/companies/totos-cover.png" alt="totos" className='img-fluid d-block mx-auto'/>
-              <button type='button' className='btn-secondary hmax mt-4 mx-auto'>
-                <Start className="fs-15"/>
-                <span className='fw-7 ms-2'>Посмотреть</span>
-              </button>
+              <Company
+                imgLogo="/imgs/companies/totos-logo.png"
+                imgCover="/imgs/companies/totos-cover.png"
+              />
             </SwiperSlide>
             <SwiperSlide>
-              <img src="/imgs/companies/yakinori-logo.png" alt="yakinori" className='img-fluid d-block mx-auto'/>
-              <img src="/imgs/companies/yakinori-cover.png" alt="yakinori" className='img-fluid d-block mx-auto'/>
-              <button type='button' className='btn-secondary hmax mt-4 mx-auto'>
-                <Start className="fs-15"/>
-                <span className='fw-7 ms-2'>Посмотреть</span>
-              </button>
+              <Company
+                imgLogo="/imgs/companies/yakinori-logo.png"
+                imgCover="/imgs/companies/yakinori-cover.png"
+              />
             </SwiperSlide>
             <SwiperSlide>
-              <img src="/imgs/companies/xiao-logo.png" alt="xiao" className='img-fluid d-block mx-auto'/>
-              <img src="/imgs/companies/xiao-cover.png" alt="xiao" className='img-fluid d-block mx-auto'/>
-              <button type='button' className='btn-secondary hmax mt-4 mx-auto'>
-                <Start className="fs-15"/>
-                <span className='fw-7 ms-2'>Посмотреть</span>
-              </button>
+              <Company
+                imgLogo="/imgs/companies/xiao-logo.png"
+                imgCover="/imgs/companies/xiao-cover.png"
+              />
             </SwiperSlide>
             <SwiperSlide>
-              <img src="/imgs/companies/dream-logo.png" alt="dream" className='img-fluid d-block mx-auto'/>
-              <img src="/imgs/companies/dream-cover.png" alt="dream" className='img-fluid d-block mx-auto'/>
-              <button type='button' className='btn-secondary hmax mt-4 mx-auto'>
-                <Start className="fs-15"/>
-                <span className='fw-7 ms-2'>Посмотреть</span>
-              </button>
+              <Company
+                imgLogo="/imgs/companies/dream-logo.png"
+                imgCover="/imgs/companies/dream-cover.png"
+              />
             </SwiperSlide>
           </Swiper>
         </Container>
