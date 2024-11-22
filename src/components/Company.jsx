be7dbd115e20f362a428ac0e3a0fbe22992f7535
Fatih <110812530+ManapovFatih@users.noreply.github.com>
2024-11-22 20:55:17 +0300

    1

--- src/components/Company.jsx
+++ src/components/Company.jsx
@@ -9,10 +9,10 @@ const Company = ({ imgLogo, imgCover, title, web, ios, android }) => {
       <img src={imgLogo} alt="logo" className="imgLogo" />
       <img src={imgCover} alt="cover" className="imgCover" />
       <h3 className="text-center">{title}</h3>
-      <div className="d-flex  justify-content-center">
-        {android && <Link to={android}><DiAndroid className="fs-22" /></Link>}
-        {ios && <Link to={ios}><DiApple className="fs-22 ms-3" /></Link>}
-        {web && <Link to={web}><DiFirefox className="fs-22 ms-3" /></Link>}
+      <div className="content">
+        {android && <Link to={android} target="_blank" ><DiAndroid /></Link>}
+        {ios && <Link to={ios} target="_blank"><DiApple /></Link>}
+        {web && <Link to={web} target="_blank"><DiFirefox /></Link>}
       </div>
       {/* <button type='button' className='btn-secondary hmax mt-4 mx-auto'>
             <Start className="fs-15"/>
